bda3c0e0acf5f94cdd3f5aeeba2e031a31173351
Ada Shaw <ayshaw@g.harvard.edu>
2018-03-13 01:12:07 -0400

    My commit message

--- P41.c
+++ P41.c
@@ -4,7 +4,7 @@
 #include "openacc.h"
 #include "timing.h"
 #include "omp.h"
-#define ncells 100000000
+#define ncells 10000000
 /* --
  * Do nsweeps sweeps of Jacobi iteration on a 1D Poisson problem
  *
