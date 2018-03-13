2ea1ebd569e623a2a3d3b95b6293c903d5837b74
Ada Shaw <ayshaw@g.harvard.edu>
2018-03-13 18:45:46 -0400

    your mom

--- P41.c
+++ P41.c
@@ -5,7 +5,7 @@
 #include "timing.h"
 #include "omp.h"
 #define ncells 100000000
-#define processors 4
+#define processors 5
 /* --
  * Do nsweeps sweeps of Jacobi iteration on a 1D Poisson problem
  *
