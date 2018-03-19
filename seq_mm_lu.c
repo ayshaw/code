00ea7bf6d6391fd5b116a707d2713a679b91e56e
Ada Shaw <ayshaw@g.harvard.edu>
2018-03-19 00:07:21 -0400

    your face

---
+++ seq_mm_lu.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "timing.h"
+#define N 1500               /* matrix size */
+
+int main (int argc, char *argv[]) 
+{
+int	i, j, k;
+timing_t tstart, tend;
+double	a[N][N],           /* matrix A to be multiplied */
+	b[N][N],           /* matrix B to be multiplied */
+	c[N][N];           /* result matrix C */
+
+
+  for (i=0; i<N; i++)
+    for (j=0; j<N; j++)
+      a[i][j]= i+j;
+
+  for (i=0; i<N; i++)
+    for (j=0; j<N; j++)
+      b[i][j]= i*j;
+
+  for (i=0; i<N; i++)
+    for (j=0; j<N; j++)
+      c[i][j]= 0;
+
+get_time(&tstart);
+
+  for (i=0; i<N; i++)    
+    {
+    for(j=0; j<N; j++)       
+      for (k=0; k<N; k++)
+          c(i,j) += a(i,k) * b(k,j)
+          c(i+1,j) += a(i+1,k) * b(k,j)
+          c(i,j+1) += a(i,k) * b(k,j+1)
+          c(i+1,j+1) += a(i+1,k) * b(k,j+1)
+    }
+
+get_time(&tend);
+
+
+   printf("Elapsed time: %g s\n", timespec_diff(tstart, tend));
+
+  printf("*****************************************************\n");
+
+  printf("Result Matrix:\n");
+  for (i=0; i<N; i++)
+  {
+    for (j=0; j<N; j++) 
+      printf("%6.2f   ", c[i][j]);
+    printf("\n"); 
+  }
+  printf("******************************************************\n");
+  printf ("Done.\n");
+printf("Elapsed time: %g s\n", timespec_diff(tstart, tend));
+}
+
