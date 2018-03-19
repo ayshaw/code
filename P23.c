58e7e8860dc58c4a5fdb5a14e1e6e99dd2b63570
Ada Shaw <ayshaw@g.harvard.edu>
2018-03-18 23:41:05 -0400

    your face

--- P23.c
+++ P23.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include "timing.h"
-#define N 1500                /* matrix size */
-#define block 25
+#define N 1500                 /* matrix size */
 
 int main (int argc, char *argv[])
 {
-    int    i, j, k, jj, kk, c00, c01, c10, c11;
-    int n = block*(N/block);
+    int i, j, k;
     timing_t tstart, tend;
     double    a[N][N],           /* matrix A to be multiplied */
     b[N][N],           /* matrix B to be multiplied */
@@ -25,32 +23,42 @@ int main (int argc, char *argv[])
     
     for (i=0; i<N; i++)
         for (j=0; j<N; j++)
-            c[i][j]= 0;
+            c[i][j]= 0.0;
+    int bsize=15;
+    int en = bsize*(N/bsize);
+    int kk, jj;
+    double sum;
     
     get_time(&tstart);
-    
-    for (kk = 0; kk < n; kk += block) {
-        for (jj = 0; jj < n; jj += block) {
-            for (i = 0; i < N; i+=2) {
-                for (j = jj; j < jj + block; j+=2) {
-                    c00 = c01 = c10 = c11 = 0;
-                    for (k=kk; k<kk+block; k++) {
-                        c00 += a[i][k] * b[k][j];
-                        c01 += a[i][k] * b[k][j+1];
-                        c10 += a[i+1][k] * b[k][j];
-                        c11 += a[i+1][k] * b[k][j+1];
+    for (kk=0;kk<en;kk+=bsize){
+        for (jj=0;jj<en;jj+=bsize){
+            for (i=0;i<N;i++){
+                for (j=jj;j<jj+bsize;j++){
+                    sum=c[i][j];
+                    for (k=kk;k<kk+bsize;k++){
+                        sum+=a[i][k]*b[k][j];
                     }
-                    c[i][j] += c00;
-                    c[i][j+1] += c01;
-                    c[i+1][j] += c10;
-                    c[i+1][j+1] += c11;
+                    c[i][j]=sum;
                 }
             }
         }
     }
-    
     get_time(&tend);
     
+    
+    printf("Elapsed time: %g s\n", timespec_diff(tstart, tend));
+    
+    printf("*****************************************************\n");
+    
+    printf("Result Matrix:\n");
+    for (i=0; i<N; i++)
+    {
+        for (j=0; j<N; j++)
+            printf("%6.2f   ", c[i][j]);
+        printf("\n");
+    }
+    printf("******************************************************\n");
+    printf ("Done.\n");
     printf("Elapsed time: %g s\n", timespec_diff(tstart, tend));
 }
 
